dc77a201c65ee0c1ab8871266518c1f8ebcfbfad
caspgin <caspgin@users.noreply.github.com>
2017-09-25 11:11:51 -0700

    Adding comments and improving code

--- StringSet.cpp
+++ StringSet.cpp
@@ -1,9 +1,11 @@
-
 #include <string>
 #include "StringSet.h"
 
 using namespace std;
 
+
+
+// Default Constructor
 StringSet::StringSet()
 {
 	currentSize = 0;
@@ -11,102 +13,152 @@ StringSet::StringSet()
 	set = new string[maxSize];
 }
 
+
+
+// Copy Constructor
+// POST: returns a deep copy of the parameter(targetSet)
+// PARAM: targetSet = set to be copied
 StringSet::StringSet(const StringSet& targetSet)
 {
-	maxSize = 2*targetSet.size();
+	maxSize = targetSet.maxSize;
 	currentSize = targetSet.size();
 	
 	set = new string[maxSize];
-	for (int i = 0; i < targetSet.size(); i++)
+	for (int i = 0; i < currentSize; i++)
 		set[i] = targetSet.set[i];
 }
 
+
+
+// Destructor
 StringSet::~StringSet()
 {
 	delete[] set;
 
 }
 
+
+
+// inserts a string  at the end of the set(array) 
+// POST: returns true if string is inserted and false if string already exists. 
+//		 Increases currentSize by 1. Doubles the actual size if array is full. 
+// PARAM: aString = A string to be inserted.
+
 bool StringSet::insert(string aString)
 {
-	for (int index = 0; index < currentSize; index++)
-		if (aString == set[index])
-			return false;
-	
-	if (maxSize == 0)
-		maxSize = 2;
-	else if (currentSize == maxSize)
-		maxSize *= 2;
-	
-	string* temp = new string[maxSize];
-	for (int i=0; i<currentSize; i++)
-		temp[i] = set[i];
+	bool stringInserted = false;
 
-	delete[] set;
-	
-	set = temp;
-	
-	set[currentSize] = aString;
+	if (find(aString) == -1)
+	{
+		if (currentSize == maxSize)
+			maxSize *= 2;
 
-	currentSize++;
+		string* temp = new string[maxSize];
+		for (int i = 0; i<currentSize; i++)
+			temp[i] = set[i];
 
-	return true;
+		delete[] set;
+
+		set = temp;
+
+		set[currentSize] = aString;
+
+		currentSize++;
+
+		stringInserted = true;
+	}
+
+	return stringInserted;
 }
 
-bool StringSet::remove(string bString)
+
+
+// removes a string by replacing it with the end string
+// POST: returns true if string is removed and false if string does not exists. Decreases currentSize by 1.
+// PARAM: target = target string to be removed
+
+bool StringSet::remove(string target)
 {
 	bool found = false;
-	for (int index = 0; index < currentSize; index++)
-		if (bString == set[index])
-		{
-			found = true;
-			set[index] = set[currentSize - 1];
-			currentSize--;
-		}
+	int index = find(target);
+	if (index != -1)
+	{
+		found = true;
+		set[index] = set[currentSize - 1];
+		currentSize--;
+	}
 	
 	return found;
 }
 
-int StringSet::find(string cString) const
+
+
+// searches for a string in the set
+// POST: returns the index of the searched string or returns -1 if string does not exsit.
+// PARAM: targetString = string to be searched
+
+int StringSet::find(string targetString) const
 {
 	for (int i = 0; i < currentSize; i++)
-		if (cString == set[i])
+		if (targetString == set[i])
 			return i;
+	
 	return -1;
 }
+
+
+
+// returns the current size of the set
+// POST: returns the currentSize 
 
 int StringSet::size() const
 {
 	return currentSize;
 }
 
-StringSet StringSet::unions(const StringSet & aStringSet) const
+
+
+// gives an union of two sets (the calling set and the parameter set)
+// POST: returns a StringSet object with all elements without repetion and an empty object if both sets are empty.
+// PARAM: stringSetA = other set to be unioned with the calling set
+
+StringSet StringSet::unions(const StringSet & stringSetA) const
 {
 	StringSet unionSet = *this;
 	
- 	for (int i = 0; i < aStringSet.size(); i++)
-		unionSet.insert(aStringSet.set[i]);
+ 	for (int i = 0; i < stringSetA.size(); i++)
+		unionSet.insert(stringSetA.set[i]);
 		
 	return unionSet;
 }
 
-StringSet StringSet::intersection(const StringSet & bStringSet) const
+
+
+// gives an intersection of two sets (the calling set and the parameter set)
+// POST: returns a StrinSet object with common elements. returns an empty object if both sets are empty.
+// PARAM: stringSetB = set to intersect with the calling set.
+
+StringSet StringSet::intersection(const StringSet & stringSetB) const
 {
 	StringSet intersectionSet;
 	
-	for (int i = 0; i < this->size(); i++)
-	{
-		for (int j = 0; j < bStringSet.size(); j++)
-			if (this->set[i] == bStringSet.set[j])
-				intersectionSet.insert(set[i]);
-	}
-	
+	for (int i = 0; (i < this->size()) && (stringSetB.size() !=0); i++)
+		if (stringSetB.find(this->set[i]) != -1)
+			intersectionSet.insert(this->set[i]);
+
+
 	return intersectionSet;
 }
 
-StringSet StringSet::difference(const StringSet & cStringSet) const
+
+
+// gives a differnce between calling set and parameter(stringSetC) set
+// POST: returns a StringSet object with elements of calling set without the common elements.
+// PARAM: stringSetC = set to be subtracted from the calling set
+
+StringSet StringSet::difference(const StringSet & stringSetC) const
 {
-	StringSet commonSet = this->intersection(cStringSet),
+	StringSet commonSet = this->intersection(stringSetC),
 			  differenceSet = *this;
 
 	for (int i = 0; i < commonSet.size(); i++)
--- StringSet.h
+++ StringSet.h
@@ -1,38 +1,70 @@
-#ifndef STRINGSET_H
-#define STRINGSET_H
-
+#pragma once
 #include <string>
 using namespace std;
 
 class StringSet
 {
 public:
-
+	
+	// Default Constructor
 	StringSet();
 
+	// Copy Constructor
+	// POST: returns a deep copy of the parameter(targetSet)
+	// PARAM: targetSet = set to be copied
 	StringSet(const StringSet& targetSet);
 
+
+	// Destructor
 	~StringSet();
 
+
+	// inserts a string  at the end of the set(array) 
+	// POST: returns true if string is inserted and false if string already exists. 
+	//		 Increases currentSize by 1. Doubles the actual size if array is full. 
+	// PARAM: aString = A string to be inserted.
 	bool insert(string aString);
 
-	bool remove(string bString);
 
-	int find(string cString) const;
+	// removes a string by replacing it with the end string
+	// POST: returns true if string is removed and false if string does not exists. Decreases currentSize by 1.
+	// PARAM: target = target string to be removed.
+	bool remove(string target);
 
+
+	// searches for a string in the set
+	// POST: returns the index of the searched string or returns -1 if string does not exsit.
+	// PARAM: targetString = string to be searched
+	int find(string targetString) const;
+
+
+	// returns the current size of the set
+	// POST: returns the currentSize 
 	int size() const;
 
-	StringSet unions(const StringSet& aStringSet) const;
 
-	StringSet intersection(const StringSet& bStringSet) const;
+	// gives an union of two sets (the calling set and the parameter set)
+	// POST: returns a StringSet object with all elements without repetion and an empty object if both sets are empty.
+	// PARAM: stringSetA = other set to be unioned with the calling set
+	StringSet unions(const StringSet& stringSetA) const;
 
-	StringSet difference(const StringSet& cStringSet) const;
+
+	// gives an intersection of two sets (the calling set and the parameter set)
+	// POST: returns a StrinSet object with common elements. returns an empty object if both sets are empty.
+	// PARAM: stringSetB = set to intersect with the calling set.
+	StringSet intersection(const StringSet& stringSetB) const;
+
+
+	// gives a differnce between calling set and parameter(stringSetC) set
+	// POST: returns a StringSet object with elements of calling set without the common elements.
+	// PARAM: stringSetC = set to be subtracted from the calling set
+	StringSet difference(const StringSet& stringSetC) const;
+
 
 private:
 
-	int currentSize;
-	int maxSize;
-	string* set;
+	int currentSize; // number of strings stored in the array
+	int maxSize; // actual size of array
+	string* set;// a pointer to store a dynamic string array
 };
 
-#endif
